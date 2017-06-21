2e07cb98500c735f3c83681306a007eae48e6e6f
Unai Motriko <unaimotriko@gmail.com>
2017-06-21 17:22:15 +0100

    adds animation sequence to walking to move action

--- Test/ GameScene.swift
+++ Test/ GameScene.swift
@@ -23,6 +23,9 @@ class GameScene: SKScene {
     let jumpTexture = SKAction.setTexture(SKTexture(imageNamed: "zombie_jump"))
     let standTexture = SKAction.setTexture(SKTexture(imageNamed: "zombie_stand"))
     var jumpAction = SKAction()
+    let halfStep = SKAction.setTexture(SKTexture(imageNamed: "zombie_walk1"))
+    let fullStep = SKAction.setTexture(SKTexture(imageNamed: "zombie_walk2"))
+    var walkAction = SKAction()
     var isTouching = false
     var movingRight = false
     var movingLeft = false
@@ -34,12 +37,13 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         print("ive moved here")
         jumpAction = SKAction.sequence([jumpTexture, jump, standTexture])
+        walkAction = SKAction.sequence([halfStep, fullStep])
         let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         borderBody.friction = 0
         self.physicsBody = borderBody
         //        physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
     }
-    //test
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
@@ -75,6 +79,8 @@ class GameScene: SKScene {
                 let player = childNode(withName: "zombie") as! SKSpriteNode
                 if isTouching && movingRight{
                     player.run(moveRight)
+                    player.run(walkAction)
+                    
                 } else if isTouching && movingLeft{
                     player.run(moveLeft)
                 }
