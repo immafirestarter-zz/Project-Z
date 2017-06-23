cf867f7de837587390c6c1b3c2929c8548db617c
hyper0009 <shehab.ahmed.uk@gmail.com>
2017-06-23 14:58:35 +0100

    sets up tests for player set up

---
+++ TestTests/PlayerTests.swift
@@ -0,0 +1,45 @@
+//
+//  Player.swift
+//  Test
+//
+//  Created by Ahmed Shehab on 23/06/2017.
+//  Copyright © 2017 Sami. All rights reserved.
+//
+
+import XCTest
+@testable import Test
+
+
+class TestPlayer: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testPlayerSetUp() {
+        let player = Player()
+        player.setUpPlayer()
+        XCTAssert(player.physicsBody?.isDynamic == true, "player body is not dynamic")
+        XCTAssert(player.physicsBody?.affectedByGravity == true, "player body is not affected by gravity")
+        XCTAssert(player.physicsBody?.restitution == 0, "player body is not affected by gravity")
+    }
+    
+    func testPlayerMovment() {
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
