37f3e3b4aa699f54a5ac1b5d3b9fc395f982497b
Unai Motriko <unaimotriko@gmail.com>
2017-06-25 11:34:18 +0100

    First test passes, asserts key exists

---
+++ Test/Key.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  Test
+//
+//  Created by Unai Motriko on 25/06/2017.
+//  Copyright © 2017 Sami. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Key:SKSpriteNode {
+}
