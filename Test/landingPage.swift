e6fd6bae4befa7eb0e3568e7aec2ff7a822158f9
Unai Motriko <unaimotriko@gmail.com>
2017-06-25 19:02:40 +0100

    indents further

--- Test/landingPage.swift
+++ Test/landingPage.swift
@@ -11,30 +11,26 @@ import SpriteKit
 import GameplayKit
 
 class LandingPage: SKScene {
-
+    
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
-
-var startButton:SKSpriteNode = SKSpriteNode()
+    
+    var startButton:SKSpriteNode = SKSpriteNode()
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         let touchlocation = touch!.location(in: self)
         let buttonStart = childNode(withName: "Begin") as! SKSpriteNode
         if buttonStart.contains(touchlocation){
-        loadAnotherLevel (levelName: "GameScene")
-    
+            loadAnotherLevel (levelName: "GameScene")
         }
     }
- 
-    func loadAnotherLevel( levelName:String) {
-            if let scene = LandingPage(fileNamed: levelName) {
-                self.view?.presentScene(scene, transition: SKTransition.fade(withDuration: 2))
-            }
-        
     
-
-            }
-
-
+    func loadAnotherLevel( levelName:String) {
+        if let scene = LandingPage(fileNamed: levelName) {
+            self.view?.presentScene(scene, transition: SKTransition.fade(withDuration: 2))
+        }
+        
+    }
+    
 }
