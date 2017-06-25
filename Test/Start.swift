7ff7d6f3a49b78baa1f0901ebcf5c1c75f84d0ea
Sami <sami@Samis-MacBook-Pro.local>
2017-06-25 16:56:45 +0100

    Basic Landing Page added

---
+++ Test/Start.swift
@@ -0,0 +1,21 @@
+//
+//  Start.swift
+//  Test
+//
+//  Created by Sami on 25/06/2017.
+//  Copyright © 2017 Sami. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Start: SKSpriteNode {
+    
+    var goesWhere:String = ""
+    
+    func setUpStart() {
+        
+        goesWhere = self.name!
+
+    }
+}
