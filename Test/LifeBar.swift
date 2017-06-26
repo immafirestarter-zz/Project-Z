49e04ecf642bd3b6f96f042c2efbc3c97fbe28d9
Unai Motriko <unaimotriko@gmail.com>
2017-06-26 07:47:10 +0100

    adds LifeBar tests and class

---
+++ Test/LifeBar.swift
@@ -0,0 +1,17 @@
+//
+//  LifeBar.swift
+//  Test
+//
+//  Created by Unai Motriko on 26/06/2017.
+//  Copyright © 2017 Sami. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class LifeBar {
+    
+    func setUp() {
+        
+    }
+}
