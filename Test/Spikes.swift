6b050801a9b53bbe3ad74e81b285cce93d3438a2
Unai Motriko <unaimotriko@gmail.com>
2017-06-28 07:58:17 +0100

    adds case body to spikes class

--- Test/Spikes.swift
+++ Test/Spikes.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import SpriteKit
+
+class Spikes:SKSpriteNode {
+    
+    func setUp() {
+        
+        let objectSize = CGSize(width: self.texture!.size().width * 0.5, height: self.texture!.size().height * 0.5)
+        let body:SKPhysicsBody = SKPhysicsBody(texture: self.texture!, size: objectSize)
+        
+        self.physicsBody = body
+        
+        body.affectedByGravity = false
+        body.isDynamic = false
+        body.allowsRotation = false
+        
+        body.collisionBitMask = 0
+        body.categoryBitMask = BodyType.spikes.rawValue
+        body.contactTestBitMask = BodyType.player.rawValue
+    }
+    
+}
