ec1f0ae8b65d36bebad69c49a03c21f659e87ab5
Nigel Said <nigelsaid@gmail.com>
2017-06-28 18:33:20 +0100

    prep for final version

--- Test/GameViewController.swift
+++ Test/GameViewController.swift
@@ -36,9 +36,9 @@ class GameViewController: UIViewController {
                     view.presentScene(sceneNode)
                     
                     view.ignoresSiblingOrder = true
-                    view.showsPhysics = true
-                    view.showsFPS = true
-                    view.showsNodeCount = true
+//                    view.showsPhysics = true
+//                    view.showsFPS = true
+//                    view.showsNodeCount = true
                 }
             }
         }
