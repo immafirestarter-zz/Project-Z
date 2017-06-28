f17cc6f4553ce4e7de643970d72dc0e5ae2d1e45
Sami <sami7mason@yahoo.co.uk>
2017-06-28 15:58:23 +0100

    ending page started

---
+++ EndGame.swift
@@ -0,0 +1,36 @@
+//
+//  EndGame.swift
+//  Test
+//
+//  Created by Sami on 28/06/2017.
+//  Copyright © 2017 Sami. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class EndGame: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    var endButton:SKSpriteNode = SKSpriteNode()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchlocation = touch!.location(in: self)
+        let buttonEnd = childNode(withName: "end") as! SKSpriteNode
+        if buttonEnd.contains(touchlocation){
+            loadAnotherLevel (levelName: "GameScene")
+            
+        }
+    }
+    
+    func loadAnotherLevel( levelName:String) {
+        if let scene = EndPage(fileNamed: levelName) {
+            self.view?.presentScene(scene, transition: SKTransition.fade(withDuration: 2))
+        }
+    }
+}
+
